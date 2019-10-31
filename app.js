1415652c0de324459e215642c521cd1e5d40b063
Carlos Estrada <carlos.estrada@piikaaboo.com>
2019-10-30 20:55:00 -0400

    PUT/PATCH/DELETE for /article/:articleTitle

--- app.js
+++ app.js
@@ -70,7 +70,42 @@ app.route('/articles/:articleTitle')
         res.send(data);
       }
     })
-  });
+  })
+  .put((req, res) => {
+    Article.update(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      (err) => {
+        if(!err) {
+          res.send('Successfully updated article.');
+        }
+      }
+    )
+  })
+  .patch((req, res) => {
+    Article.update(
+      {title: req.params.articleTitle},
+      {$set: req.body},
+      (err) => {
+        if(!err) {
+          res.send('Successfully updated article.');
+        }
+      }
+    )
+  })
+  .delete((req, res) => {
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      (err) => {
+      if(!err) {
+        res.send('Successfully deleted all articles.');
+      }
+      else {
+        res.send(err);
+      }
+    })
+  })
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
